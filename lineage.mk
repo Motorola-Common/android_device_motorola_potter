57e26030f7b6ce4071dec17c278d23d18f0777cb
Pedro Kalil <kalilgamer0@gmail.com>
2018-06-21 03:38:34 +0000

    potter: utilize moto msm8953-common trees

--- lineage.mk
+++ lineage.mk
@@ -16,7 +16,11 @@
 
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/motorola/potter/full_potter.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from potter device
+$(call inherit-product, device/motorola/potter/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -30,6 +34,7 @@ PRODUCT_DEVICE := potter
 PRODUCT_NAME := lineage_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
+PRODUCT_MODEL := Moto G5 Plus
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -38,3 +43,6 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto G5 Plus"
+
+# for specific
+$(call inherit-product, vendor/motorola/potter/potter-vendor.mk)
