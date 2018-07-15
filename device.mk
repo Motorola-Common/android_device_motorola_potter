3f6f711ed3c03850322bc2a1ae5f52d900766c1a
Pedro Kalil <kalilgamer0@gmail.com>
2018-07-15 23:36:50 +0000

    potter: import Moto Sensor Hub * Montana doesnt use it, therefore it cant be in the common tree

--- device.mk
+++ device.mk
@@ -84,6 +84,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
 
+PRODUCT_PACKAGES += \
+    libsensorhub \
+    motosh \
+    sensorhub.$(TARGET_BOARD_PLATFORM) \
+    sensors.$(TARGET_BOARD_PLATFORM) \
+    sensors.rp \
+    sensors.tof \
+    sensors.tof.vl53l0
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-potter.conf:system/vendor/etc/thermal-engine.conf
